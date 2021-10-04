96309a8357013327efb55f5b6852f6f797465e4a
Anand <anand@dheemantech.com>
2021-10-04 20:16:41 +0550

    updated changes - 2021-10-04 19:56:41

---
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -0,0 +1,89 @@
+
+package com.crio.warmup.stock;
+
+
+import com.crio.warmup.stock.dto.AnnualizedReturn;
+import com.crio.warmup.stock.dto.PortfolioTrade;
+import com.crio.warmup.stock.dto.TotalReturnsDto;
+import com.crio.warmup.stock.log.UncaughtExceptionHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.web.client.RestTemplate;
+
+
+public class PortfolioManagerApplication {
+
+
+
+
+  // TODO: CRIO_TASK_MODULE_CALCULATIONS
+  //  Now that you have the list of PortfolioTrade and their data, calculate annualized returns
+  //  for the stocks provided in the Json.
+  //  Use the function you just wrote #calculateAnnualizedReturns.
+  //  Return the list of AnnualizedReturns sorted by annualizedReturns in descending order.
+
+  // Note:
+  // 1. You may need to copy relevant code from #mainReadQuotes to parse the Json.
+  // 2. Remember to get the latest quotes from Tiingo API.
+
+  public static List<AnnualizedReturn> mainCalculateSingleReturn(String[] args)
+      throws IOException, URISyntaxException {
+     return Collections.emptyList();
+  }
+
+  // TODO: CRIO_TASK_MODULE_CALCULATIONS
+  //  Return the populated list of AnnualizedReturn for all stocks.
+  //  Annualized returns should be calculated in two steps:
+  //   1. Calculate totalReturn = (sell_value - buy_value) / buy_value.
+  //      1.1 Store the same as totalReturns
+  //   2. Calculate extrapolated annualized returns by scaling the same in years span.
+  //      The formula is:
+  //      annualized_returns = (1 + total_returns) ^ (1 / total_num_years) - 1
+  //      2.1 Store the same as annualized_returns
+  //  Test the same using below specified command. The build should be successful.
+  //     ./gradlew test --tests PortfolioManagerApplicationTest.testCalculateAnnualizedReturn
+
+  public static AnnualizedReturn calculateAnnualizedReturns(LocalDate endDate,
+      PortfolioTrade trade, Double buyPrice, Double sellPrice) {
+      return new AnnualizedReturn("", 0.0, 0.0);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static void main(String[] args) throws Exception {
+    Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler());
+    ThreadContext.put("runId", UUID.randomUUID().toString());
+
+
+
+    printJsonObject(mainCalculateSingleReturn(args));
+
+  }
+}
+
---
+++ qmoney/src/test/optional-java/com/crio/warmup/stock/ModuleOneTestNew.java
@@ -0,0 +1,39 @@
+
+package com.crio.warmup.stock;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ModuleOneTestNew {
+
+    @Test
+    void mainReadFile() throws Exception {
+        //given
+        String filename = "assessments/trades.json";
+        List<String> expected = Arrays.asList(new String[]{"MSFT", "CSCO", "CTS"});
+
+        //when
+        List<String> results = PortfolioManagerApplication
+                .mainReadFile(new String[]{filename});
+
+        //then
+        Assertions.assertEquals(expected, results);
+    }
+
+    @Test
+    void mainReadFileEdgecase() throws Exception {
+        //given
+        String filename = "assessments/empty.json";
+        List<String> expected = Arrays.asList(new String[]{});
+
+        //when
+        List<String> results = PortfolioManagerApplication
+                .mainReadFile(new String[]{filename});
+
+        //then
+        Assertions.assertEquals(expected, results);
+    }
+
+}
