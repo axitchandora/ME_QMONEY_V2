424d773d9f7b74b46ff6d2f66647da94f6a7c50b
Anand <anand@dheemantech.com>
2021-10-04 18:53:54 +0550

    updated changes - 2021-10-04 18:33:54

---
+++ qmoney/src/main/java/com/crio/warmup/stock/PortfolioManagerApplication.java
@@ -0,0 +1,148 @@
+
+package com.crio.warmup.stock;
+
+
+import com.crio.warmup.stock.dto.AnnualizedReturn;
+import com.crio.warmup.stock.dto.PortfolioTrade;
+import com.crio.warmup.stock.log.UncaughtExceptionHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.web.client.RestTemplate;
+
+
+public class PortfolioManagerApplication {
+
+  // TODO: CRIO_TASK_MODULE_JSON_PARSING
+  //  Task:
+  //       - Read the json file provided in the argument[0], The file is available in the classpath.
+  //       - Go through all of the trades in the given file,
+  //       - Prepare the list of all symbols a portfolio has.
+  //       - if "trades.json" has trades like
+  //         [{ "symbol": "MSFT"}, { "symbol": "AAPL"}, { "symbol": "GOOGL"}]
+  //         Then you should return ["MSFT", "AAPL", "GOOGL"]
+  //  Hints:
+  //    1. Go through two functions provided - #resolveFileFromResources() and #getObjectMapper
+  //       Check if they are of any help to you.
+  //    2. Return the list of all symbols in the same order as provided in json.
+
+  //  Note:
+  //  1. There can be few unused imports, you will need to fix them to make the build pass.
+  //  2. You can use "./gradlew build" to check if your code builds successfully.
+
+  public static List<String> mainReadFile(String[] args) throws IOException, URISyntaxException {
+
+     return Collections.emptyList();
+  }
+
+
+  // Note:
+  // 1. You may need to copy relevant code from #mainReadQuotes to parse the Json.
+  // 2. Remember to get the latest quotes from Tiingo API.
+
+
+
+
+
+  // Note:
+  // 1. You may have to register on Tiingo to get the api_token.
+  // 2. Look at args parameter and the module instructions carefully.
+  // 2. You can copy relevant code from #mainReadFile to parse the Json.
+  // 3. Use RestTemplate#getForObject in order to call the API,
+  //    and deserialize the results in List<Candle>
+
+
+
+  private static void printJsonObject(Object object) throws IOException {
+    Logger logger = Logger.getLogger(PortfolioManagerApplication.class.getCanonicalName());
+    ObjectMapper mapper = new ObjectMapper();
+    logger.info(mapper.writeValueAsString(object));
+  }
+
+  private static File resolveFileFromResources(String filename) throws URISyntaxException {
+    return Paths.get(
+        Thread.currentThread().getContextClassLoader().getResource(filename).toURI()).toFile();
+  }
+
+  private static ObjectMapper getObjectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
+  }
+
+
+  // TODO: CRIO_TASK_MODULE_JSON_PARSING
+  //  Follow the instructions provided in the task documentation and fill up the correct values for
+  //  the variables provided. First value is provided for your reference.
+  //  A. Put a breakpoint on the first line inside mainReadFile() which says
+  //    return Collections.emptyList();
+  //  B. Then Debug the test #mainReadFile provided in PortfoliomanagerApplicationTest.java
+  //  following the instructions to run the test.
+  //  Once you are able to run the test, perform following tasks and record the output as a
+  //  String in the function below.
+  //  Use this link to see how to evaluate expressions -
+  //  https://code.visualstudio.com/docs/editor/debugging#_data-inspection
+  //  1. evaluate the value of "args[0]" and set the value
+  //     to the variable named valueOfArgument0 (This is implemented for your reference.)
+  //  2. In the same window, evaluate the value of expression below and set it
+  //  to resultOfResolveFilePathArgs0
+  //     expression ==> resolveFileFromResources(args[0])
+  //  3. In the same window, evaluate the value of expression below and set it
+  //  to toStringOfObjectMapper.
+  //  You might see some garbage numbers in the output. Dont worry, its expected.
+  //    expression ==> getObjectMapper().toString()
+  //  4. Now Go to the debug window and open stack trace. Put the name of the function you see at
+  //  second place from top to variable functionNameFromTestFileInStackTrace
+  //  5. In the same window, you will see the line number of the function in the stack trace window.
+  //  assign the same to lineNumberFromTestFileInStackTrace
+  //  Once you are done with above, just run the corresponding test and
+  //  make sure its working as expected. use below command to do the same.
+  //  ./gradlew test --tests PortfolioManagerApplicationTest.testDebugValues
+
+  public static List<String> debugOutputs() {
+
+     String valueOfArgument0 = "trades.json";
+     String resultOfResolveFilePathArgs0 = "";
+     String toStringOfObjectMapper = "";
+     String functionNameFromTestFileInStackTrace = "";
+     String lineNumberFromTestFileInStackTrace = "";
+
+
+    return Arrays.asList(new String[]{valueOfArgument0, resultOfResolveFilePathArgs0,
+        toStringOfObjectMapper, functionNameFromTestFileInStackTrace,
+        lineNumberFromTestFileInStackTrace});
+  }
+
+
+  // Note:
+  // Remember to confirm that you are getting same results for annualized returns as in Module 3.
+
+
+
+  public static void main(String[] args) throws Exception {
+    Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler());
+    ThreadContext.put("runId", UUID.randomUUID().toString());
+
+    printJsonObject(mainReadFile(args));
+
+
+
+  }
+}
+
---
+++ qmoney/src/test/java/com/crio/warmup/stock/PortfolioManagerApplicationTest.java
@@ -0,0 +1,46 @@
+
+package com.crio.warmup.stock;
+
+import com.crio.warmup.stock.dto.PortfolioTrade;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PortfolioManagerApplicationTest {
+
+
+  // TODO: WARNING!!!
+  //  DO NOT MODIFY ANY FILES IN THE TESTS/ ASSESSMENTS UNLESS ASKED TO.
+  //  These files are replaced from stock contents while executing the assessments.
+  //  Any modifications in this file may result in Assessment failure!
+
+
+  @Test
+  void mainReadFile() throws Exception {
+    //given
+    String filename = "trades.json";
+    List<String> expected = Arrays.asList(new String[]{"AAPL", "MSFT", "GOOGL"});
+
+    //when
+    List<String> results = PortfolioManagerApplication
+        .mainReadFile(new String[]{filename});
+
+    //then
+    Assertions.assertEquals(expected, results);
+  }
+
+
+
+
+
+  @Test
+  public void testDebugValues() {
+    List<String> responses = PortfolioManagerApplication.debugOutputs();
+    Assertions.assertTrue(responses.get(0).contains("trades.json"));
+  }
+
+}
+
